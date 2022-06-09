158bdfa02401cb7758dff1b628736e87454fc6f9
renzhong <lu631992923q@gmail.com>
2022-06-09 23:23:24 +0200

    delete test.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
     # time_cheapest_insertion = chp_1.cal_time(chp_1.cheapest_insertion,graph_1)
     # print("Elapsed time of 1st Algorithms: ",time_nearest_neighbor)
     # print("Elapsed time of 2nd Algorithms: ",time_cheapest_insertion)
+
+
     chp_1 = HCP("./Daten/eins.txt")
     g = chp_1.graph
     mst = MST()
